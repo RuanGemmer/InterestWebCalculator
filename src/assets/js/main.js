52a65395b9b4862f81914ead4f55516c35c770b4
RuanGemmer <ruangemmer@live.com>
2023-08-24 09:54:09 -0300

    Imported static files to render

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,4 +1,14 @@
+import "../css/remedy.css";
+import "../css/style.css";
+import "../img/Calculator-white.svg";
+import "../img/Calculator.svg";
+import "../img/credit-card.svg";
+import "../img/exponential.svg";
+import "../img/linear.svg";
+import "../img/money.svg";
+import "../img/percent.svg";
 import { simpleInterst } from "./simple_interest";
+
 function currentPage() {
     const currentPageOpen = window.location.pathname;
     const currentPageName = currentPageOpen.substring(
